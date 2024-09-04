2cc9ad1c4a00a4bb47bf79a0eee520ec639eacf9
mallasaugat <saugat.smalla@gmail.com>
2024-09-03 22:25:03 -0700

    VGG, NiN

--- cnn.py
+++ cnn.py
@@ -89,23 +89,116 @@ class LeNet(Classifier):
 ## AlexNet
 class AlexNet(Classifier):
     """AlexNet Class"""
+
     def __init__(self, lr=0.1, num_classes=10):
         super().__init__()
         self.save_hyperparameters()
         self.net = nn.Sequential(
             nn.LazyConv2d(96, kernel_size=11, stride=4, padding=1),
-            nn.ReLU(), nn.MaxPool2d(kernel_size=3, stride=2),
-            nn.LazyConv2d(256, kernel_size=5, padding=2), nn.ReLU(),
+            nn.ReLU(),
             nn.MaxPool2d(kernel_size=3, stride=2),
-            nn.LazyConv2d(384, kernel_size=3, padding=1), nn.ReLU(),
-            nn.LazyConv2d(384, kernel_size=3, padding=1), nn.ReLU(),
-            nn.LazyConv2d(256, kernel_size=3, padding=1), nn.ReLU(),
-            nn.MaxPool2d(kernel_size=3, stride=2), nn.Flatten(),
-            nn.LazyLinear(4096), nn.ReLU(), nn.Dropout(p=0.5),
-            nn.LazyLinear(4096), nn.ReLU(),nn.Dropout(p=0.5),
-            nn.LazyLinear(num_classes))    )
+            nn.LazyConv2d(256, kernel_size=5, padding=2),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=3, stride=2),
+            nn.LazyConv2d(384, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.LazyConv2d(384, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.LazyConv2d(256, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=3, stride=2),
+            nn.Flatten(),
+            nn.LazyLinear(4096),
+            nn.ReLU(),
+            nn.Dropout(p=0.5),
+            nn.LazyLinear(4096),
+            nn.ReLU(),
+            nn.Dropout(p=0.5),
+            nn.LazyLinear(num_classes),
+        )
         self.net.apply(init_cnn)
 
 
 # Layer Summary
 # print(AlexNet().layer.summary((1,1,224,224)))
+
+
+## VGG Blocks
+
+
+def vgg_block(num_convs, out_channels):
+    layers = []
+    for _ in range(num_convs):
+        layers.append(nn.LazyConv2d(out_channels, kernel_size=3, padding=1))
+        layers.append(nn.ReLU())
+
+    layers.append(nn.MaxPool2d(kernel_size=2, stride=2))
+    return nn.Sequential(*layers)
+
+
+class VGG(Classifier):
+    """VGG Network"""
+
+    def __init__(self, arch, lr=0.1, num_classes=10):
+        self.save_hyperparameters()
+        conv_blks = []
+
+        for num_convs, out_channels in arch:
+            conv_blks.append(vgg_block(num_convs, out_channels))
+
+        self.net = nn.Sequential(
+            *conv_blks,
+            nn.Flatten(),
+            nn.LazyLinear(4096),
+            nn.ReLU(),
+            nn.Dropout(0.5),
+            nn.LazyLinear(4096),
+            nn.ReLU(),
+            nn.Dropout(0.5),
+            nn.LazyLinear(num_classes)
+        )
+        self.net.apply(init_cnn)
+
+
+## VGG Sumamry
+## print(VGG(arch=((1, 64), (1, 128), (2, 256), (2, 512), (2, 512))).layer_summary((1, 1, 224, 224)))
+
+
+## NiN Network
+
+
+def nin_block(out_channels, kernel_size, strides, padding):
+    """NiN Block"""
+    return nn.Sequential(
+        nn.LazyConv2d(out_channels, kernel_size, strides, padding),
+        nn.ReLU(),
+        nn.LazyConv2d(out_channels, kernel_size=1),
+        nn.ReLU(),
+        nn.LazyConv2d(out_channels, kernel_size=1),
+        nn.ReLU(),
+    )
+
+
+class Nin(Classifier):
+    """NiN Class"""
+
+    def __init__(self, lr=0.1, num_classes=10):
+        suepr().__init__()
+        self.save_hyperparameters()
+        self.net = nn.Sequential(
+            nin_block(96, kernel_size=11, strides=4, padding=0),
+            nn.MaxPool2d(3, stride=2),
+            nin_block(256, kernel_size=5, strides=1, padding=2),
+            nn.MaxPool2d(3, stride=2),
+            nin_block(384, kernel_size=3, strides=1, padding=1),
+            nn.MaxPool2d(3, stride=2),
+            nn.Dropout(0.5),
+            nin_block(num_classes, kernel_size=3, strides=1, padding=1),
+            nn.AdaptiveAvgPool2d((1, 1)),
+            nn.Flatten(),
+        )
+        self.net.apply(init_cnn)
+
+
+## NiN Block Summary
+# print(NiN().layer_summary((1, 1, 224, 224)))
